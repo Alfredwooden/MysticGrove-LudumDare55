5c0afc19958644658f77980474bd89fbc2f5b3a3
Alfredo Barros <coomcs@gmail.com>
2024-04-15 00:20:03 +1200

    Updated enemy

--- Scripts/Human.gd
+++ Scripts/Human.gd
@@ -4,37 +4,60 @@ extends CharacterBody2D
 @export var limit = 0.5
 
 @onready var animations = $AnimatedSprite2D
+@onready var changeStateTimer = $Timers/ChangeState
+@onready var movingTimer = $Timers/Moving
 
 var player_chase = false
 var player = null
-var startPosition
-var endPosition
+
+var idle = false
+var walking = false
+var xdir = 1
+var ydir = 1
+var motion = Vector2()
+var moving_vertical_horizontal = 1
 
 func _ready():
-	startPosition = position
-	endPosition = startPosition + Vector2(3 * 32, 3 * 16)
-
-func changeDirection():
-	var tempEnd = endPosition
-	endPosition = startPosition
-	startPosition = tempEnd
+	walking = true
+	randomize()
+	changeStateTimer.start()
+	movingTimer.start()
 
 func updateVelocity():
-	var moveDirection = (endPosition - position).normalized() * speed
-	
 	if player_chase and is_instance_valid(player):
-		moveDirection = (player.position - position).normalized() * speed
+		motion = (player.position - position).normalized() * speed
+	else:
+		if walking:
+			if moving_vertical_horizontal == 1:
+				if xdir == -1:
+					animations.flip_h = true
+				if xdir == 1:
+					animations.flip_h = false
+				motion.x = speed * xdir
+				motion.y = 0
+			elif moving_vertical_horizontal == 2:
+				motion.y = speed * ydir
+				motion.x = 0
+		else:
+			motion.x = 0
+			motion.y = 0
 	
-	if moveDirection.length() < limit:
-		changeDirection()
+	if not player_chase and motion.length() < limit:
+		if walking:
+			motion = motion.normalized() * speed
+		else:
+			motion = Vector2.ZERO
 	
-	velocity = moveDirection
+	velocity = motion
 
 func updateAnimation():
-	if velocity.length() > 0:
-		animations.play("Walk")
+	if player_chase and is_instance_valid(player):
+		animations.play("Chasing")
 	else:
-		animations.stop()
+		if walking:
+			animations.play("Walk")
+		else:
+			animations.play("Idle")
 
 func _physics_process(delta):
 	updateVelocity()
@@ -50,3 +73,43 @@ func _on_detection_area_body_exited(body):
 	if body == player:
 		player = null
 		player_chase = false
+
+func _on_ChangeStateTimer_timeout():
+	var waitTime = 1
+	
+	if walking:
+		idle = true
+		walking = false
+		waitTime = randf_range(1, 5)
+	else:
+		walking = true
+		idle = false
+		waitTime = randf_range(2, 6)
+		
+		var x = randf_range(1, 2)
+		if x > 1.5:
+			moving_vertical_horizontal = 1
+		else:
+			moving_vertical_horizontal = 2
+	
+	changeStateTimer.wait_time = waitTime
+	changeStateTimer.start()
+
+func _on_Moving_timeout():
+	var x = randf_range(1, 2)
+	var y = randf_range(1, 2)
+	var waitTime = randf_range(1, 4)
+	
+	if x > 1.5:
+		xdir = 1
+	else:
+		xdir = -1
+	
+	if y > 1.5:
+		ydir = 1
+	else:
+		ydir = -1
+	
+	movingTimer.wait_time = waitTime
+	movingTimer.start()
+	print("WalkingTimer started. xdir: ", xdir, " ydir: ", ydir, " Wait time: ", waitTime)
