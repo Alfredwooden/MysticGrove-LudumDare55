fe5c819f781d40a0bccff271b267bcd814133618
Alfredo Barros <coomcs@gmail.com>
2024-04-14 19:17:08 +1200

    Updated sorting

--- Scripts/Human.gd
+++ Scripts/Human.gd
@@ -1,4 +1,3 @@
-# Human.gd
 extends CharacterBody2D
 
 @export var speed = 20
@@ -11,7 +10,8 @@ var endPosition
 
 func _ready():
 	startPosition = position
-	endPosition = startPosition + Vector2(0, 3*16)
+	# Adjust endPosition for isometric movement, x changes affect y
+	endPosition = startPosition + Vector2(3 * 32, 3 * 16)  # Move 3 tiles in x and 1.5 in y
 
 func changeDirection():
 	var tempEnd = endPosition
@@ -21,15 +21,20 @@ func changeDirection():
 func updateVelocity():
 	var moveDirection = endPosition - position
 	if moveDirection.length() < limit:
-			changeDirection()
+		changeDirection()
+	# Ensure the movement vector is adapted to isometric perspective
 	velocity = moveDirection.normalized() * speed
 
 func updateAnimation():
 	var animationString = "Walk"
-	if velocity.y > 0:
-		animationString = "Walk"
+	# Decide the animation based on direction using GDScript's conditional expression format
+	if velocity.x != 0:
+		animationString = "Walk_Right" if velocity.x > 0 else "Walk_Left"
+	elif velocity.y != 0:
+		animationString = "Walk_Down" if velocity.y > 0 else "Walk_Up"
 
 	animations.play(animationString)
+
 
 func _physics_process(delta):
 	updateVelocity()
--- Scripts/World.gd
+++ Scripts/World.gd
@@ -2,7 +2,7 @@
 extends Node2D
 
 @onready var heartContainer = $UI/Control/MarginContainer/Hearths/HeartsContainer
-@onready var player = $Player
+@onready var player = $Isometric_Village/Player
 
 const camera_position_1_x = 191
 const camera_position_1_y = 97
@@ -14,23 +14,8 @@ func _ready():
 	heartContainer.setMaxHearts(player.maxHealth)
 	heartContainer.updateHearts(player.currentHealth)	
 	player.healthChanged.connect(heartContainer.updateHearts)
-	$Area_2/ColorRect.size.x = 50
 
 func _physics_process(delta):
 	$UI/Control/MarginContainer/Souls/SoulsContainer/Souls_GUI/SoulsLabel.text  = "= " + str(Global.get_soul_coins())
 	$UI/Control/MarginContainer/Souls/SoulsContainer/Skulls_GUI/SkullLabel.text  = "= " + str(Global.get_skull_souls())
 	$UI/Control/MarginContainer/Souls/SoulsContainer/Ghosts_GUI/GhostLabel.text  = "= " + str(Global.get_ghost_souls())
-
-	if Global.camera_pos == 1:
-		$Camera2D.position.x = camera_position_1_x
-		$Camera2D.position.y = camera_position_1_y
-	elif Global.camera_pos == 2:
-		$Camera2D.position.x = camera_position_2_x
-		$Camera2D.position.y = camera_position_2_y
-
-func _on_openFarmingZone2_body_entered(body):
-	if body.has_method("player"):
-		if not $Area_2/openFarmingZone2/CollisionShape2D.disabled:
-			Global.camera_pos = 2
-			$Area_2/AnimationPlayer.play("SpawnField")
-			$Area_2/openFarmingZone2/CollisionShape2D.disabled = true
